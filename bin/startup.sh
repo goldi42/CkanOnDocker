c0f2a6c7c56755db2732cc2e9d29ac6f1bca507d
goldi42 <113867+goldi42@users.noreply.github.com>
2017-07-29 09:14:52 +0200

    add startup script with ckanbuilder

---
+++ bin/startup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -eu -o pipefail
+
+config_file="config/${environment}.ini"
+
+echo "Waiting for other services to start CKAN"
+
+echo "Waiting for solr"
+until $(nc -zv solr 8983); do
+    printf '.'
+    sleep 5
+done
+
+echo "Waiting for postgres"
+until $(nc -zv db 5432); do
+    printf '.'
+    sleep 5
+done
+
+echo "All servies are up starting CKAN..."
+
+echo "Install CKAN extensions"
+ckanbuilder install extensions
+
+echo "Build Assets"
+ckanbuilder build assets
+
+echo "Activate CKAN plugins"
+ckanbuilder configure plugins --configini_file ${config_file}
+
+echo "Initialise database"
+if [ $init_database -eq 1 ]; then
+paster --plugin=ckan db init -c ${config_file}
+fi
+
+echo "Start application server"
+gunicorn --paste ${config_file} --reload
